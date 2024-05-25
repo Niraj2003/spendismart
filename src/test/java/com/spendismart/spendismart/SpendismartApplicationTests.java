e9dc5a02016c2f241dc5e84394b310558f108c83
Niraj Amrutkar <>
2024-05-25 14:48:51 +0530

    Initialized

---
+++ src/test/java/com/spendismart/spendismart/SpendismartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spendismart.spendismart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendismartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
